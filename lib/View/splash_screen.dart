2f74b9ea6e66c50551564e709097b1f43abfb2fa
Thunt77 <thuno2698@gmail.com>
2022-09-13 22:49:08 +0700

    fix svg bug splash screen

--- lib/View/splash_screen.dart
+++ lib/View/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:project1/View/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -24,7 +23,7 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset('lib/Assets/Splash screen.svg'),
+        child: Image.asset('lib/Assets/Splash screen.png'),
       ),
     );
   }
